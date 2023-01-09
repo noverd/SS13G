48efe58a9da641fb6445a56306f7ec71575545d5
noverd <dasfasru@gmail.com>
2023-01-09 15:01:54 +0300

    Update LogSys

--- scripts/log.gd
+++ scripts/log.gd
@@ -1,8 +1,10 @@
 extends Node
 
-func l(data, level="INFO"):
-	print("[%s][%s] %s" % [level, Time.get_time_string_from_system(), data])
+func l(data, level="[color=green]INFO[/color]"):
+	print_rich("[%s][%s] %s" % [level, Time.get_time_string_from_system(), data])
 
 func err(err, con: bool = false):
-	assert(con, err)
+	if con:
+		return
+	print_rich("[[color=red]ERROR[/color]][%s] %s" % [Time.get_time_string_from_system(), err])
 	
