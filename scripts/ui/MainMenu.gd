0a2e2012f40d75bf20effd80ba8a064fa2f787de
Noverd/Gagarinten <dasfasru@gmail.com>
2023-02-12 20:20:26 +0300

    Fix default russian translate

--- scripts/ui/MainMenu.gd
+++ scripts/ui/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _ready():
-	TranslationServer.set_locale("ru")
+	TranslationServer.set_locale("en")
 	for i in OS.get_cmdline_args():
 		if i == "--server":
 			get_tree().change_scene_to_file("res://scenes/test.tscn")
